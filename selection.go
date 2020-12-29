84f29f2cc2758ff7dcee9e386004c132bfd66e3f
Kevin C. Krinke <kevin@krinke.ca>
2020-12-29 11:28:33 -0500

    selection.go deleted, belongs in CTK

--- selection.go
+++
@@ -1,8 +0,0 @@
-package cdk
-
-// manages selection of text
-// - could be combined with xcut (if local)
-// - owned by the widget containing the text selected
-// - maps to the widget's render buffer
-// - doesn't actually store text itself
-// - overlay styles when selected?
