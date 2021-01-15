0c940b29c16e48c5f947c9f1f1126edc64df3bdc
Kevin C. Krinke <kevin@krinke.ca>
2021-01-15 02:19:44 -0500

    build-fix updates to cdk-demo

--- _demos/cdk-demo.go
+++ _demos/cdk-demo.go
@@ -24,13 +24,13 @@ func (w *CdkDemoWindow) Draw(canvas cdk.Canvas) cdk.EventFlag {
 	w.LogInfo("Draw: %s", canvas)
 	theme := w.GetDisplayManager().DefaultTheme()
 	size := canvas.GetSize()
-	canvas.Box(cdk.Point2I{}, size, true, true, theme)
+	canvas.Box(cdk.Point2I{}, size, true, true, false, ' ', theme.Content.Normal, theme.Border.Normal, theme.Border.BorderRunes)
 	content := "<b><u>H</u><span foreground=\"gold\">ello</span> <i>W</i><span foreground=\"brown\">orld</span></b>\n"
 	content += "<span foreground=\"grey\" background=\"cyan\">(press CTRL+c to exit)</span>\n"
 	content += "<span foreground=\"silver\" background=\"darkblue\">" + time.Now().Format("2006-01-02 15:04:05") + "</span>"
 	textPoint := cdk.MakePoint2I(size.W/2/2, size.H/2-1)
 	textSize := cdk.MakeRectangle(size.W/2, size.H/2)
-	canvas.DrawText(textPoint, textSize, cdk.JUSTIFY_CENTER, false, cdk.WRAP_WORD, cdk.DefaultColorTheme.Normal, true, content)
+	canvas.DrawText(textPoint, textSize, cdk.JUSTIFY_CENTER, false, cdk.WRAP_WORD, false, theme.Content.Normal, true, content)
 	return cdk.EVENT_STOP
 }
 
